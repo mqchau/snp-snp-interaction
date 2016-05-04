055161f76fb462322689630e30f5777f9a729d6f
Quan Chau <quanqchau@gmail.com>
2016-05-03 19:51:39 -0700

    timed performance of apriori methods and pending baseline method

--- baseline_method.r
+++ baseline_method.r
@@ -1,53 +1,39 @@
-library(foreach)
-library(doParallel)
-cl<-makeCluster(4)
-registerDoParallel(cl)
 
-all_data <- read.csv("datafile/sample2.csv", head=FALSE, sep=",")
+time_execution <- function(sample_idx){
+    sample_name = sprintf("datafile/sample%d.csv", sample_idx)
+    all_data <- read.csv(sample_name, head=FALSE, sep=",")
 
-num_snp = ncol(all_data) - 1
-num_sample = nrow(all_data)
-case_control_col = ncol(all_data)
-num_case = length(which(all_data[,case_control_col] == 1))
-num_control = length(which(all_data[,case_control_col] == 0))
+    # start timing after reading all data into memory
+    ptm <- proc.time()
 
-all_snp_combination = combn(num_snp, 2)
+    num_snp = ncol(all_data)
+    num_case = nrow(all_data)
 
-percent_case_explained <- function(snp_comb){
-    num_case_match = length(which(all_data[,snp_comb[1]] == 1 & 
-                                  all_data[,snp_comb[2]] == 1 & 
-                                  all_data[,case_control_col] == 1))
-    return(num_case_match/num_case)
+    all_snp_combination = combn(num_snp, 2)
+
+    percent_case_explained <- function(snp_comb){
+        num_case_match = length(which(all_data[,snp_comb[1]] == 1 & 
+                                      all_data[,snp_comb[2]] == 1))
+        return(num_case_match/num_case)
+    }
+
+    num_combination = ncol(all_snp_combination)
+    all_snp_comb_frame = data.frame(snp1=integer(num_combination),
+                                   snp2=integer(num_combination),
+                                   case_percent=double(num_combination))
+
+    for (comb_idx in 1:ncol(all_snp_combination)){
+        all_snp_comb_frame$snp1[comb_idx] = all_snp_combination[1,comb_idx]
+        all_snp_comb_frame$snp2[comb_idx] = all_snp_combination[2,comb_idx]
+        all_snp_comb_frame$case_percent[comb_idx] = percent_case_explained(all_snp_combination[1:2, comb_idx])
+    }
+
+    explained_threshold = 0.80
+    probable_snp_comb_case = subset(all_snp_comb_frame, case_percent > explained_threshold)
+
+
+    return(proc.time() - ptm)
 }
 
-percent_control_explained <- function(snp_comb){
-    num_case_match = length(which(all_data[,snp_comb[1]] == 1 & 
-                                  all_data[,snp_comb[2]] == 1 & 
-                                  all_data[,case_control_col] == 0))
-    return(num_case_match/num_control)
-}
-
-num_combination = ncol(all_snp_combination)
-all_snp_comb_frame = data.frame(snp1=integer(num_combination),
-                               snp2=integer(num_combination),
-                               case_percent=double(num_combination),
-                               control_percent=double(num_combination))
-
-all_snp_comb<-foreach(comb=all_snp_combination, .combine='cbind') %dopar%{
-    snp1 = comb[1]
-    snp2 = comb[2]
-    case_percent = percent_case_explained(comb[1:2])
-    control_percent = percent_control_explained(comb[1:2])
-    c(snp1, snp2, case_percent, control_percent)
-}
-stopCluster(cl)
-
-all_snp_comb = t(all_snp_comb)
-
-explained_threshold = 0.80
-probable_snp_comb_case = subset(all_snp_comb, all_snp_comb[,3] > explained_threshold)
-probable_snp_comb_control = subset(all_snp_comb, all_snp_comb[,4] > explained_threshold)
-probable_snp_comb_case
-probable_snp_comb_control
-
+print(time_execution(2))
 
--- create_sample_datafile.py
+++ create_sample_datafile.py
@@ -12,21 +12,23 @@ from random import randint
 import random
 
 if __name__ == "__main__":
-    num_snp = 100
-    num_case = 100
-    percent_perfect_correlation = 0.9
-    target_snp = [10,15]
+    for sample_idx in range(4):
+        num_snp = 500 * 10**sample_idx
+        print("generating sample of %d snp", num_snp)
+        num_case = 100
+        percent_perfect_correlation = 0.9
+        target_snp = [10,15]
 
-    data_array = np.zeros((num_case, num_snp), dtype=np.uint8)
+        data_array = np.zeros((num_case, num_snp), dtype=np.uint8)
 
-    # generate the case sample, control sample are not necessary
-    # for now let's just assume snp1 and snp2 must be 1,1 together to activate
-    for i in range(num_case):
-        for j in range(num_snp):
-            if j not in target_snp or random.random() > percent_perfect_correlation:
-                data_array[i,j] = randint(0,1) 
-            else:
-                data_array[i,j] = 1
+        # generate the case sample, control sample are not necessary
+        # for now let's just assume snp1 and snp2 must be 1,1 together to activate
+        for i in range(num_case):
+            for j in range(num_snp):
+                if j not in target_snp or random.random() > percent_perfect_correlation:
+                    data_array[i,j] = randint(0,1) 
+                else:
+                    data_array[i,j] = 1
 
-    np.savetxt("datafile/sample1.csv", data_array, delimiter=",", fmt='%i')
+        np.savetxt("datafile/sample%d.csv" % (sample_idx+1), data_array, delimiter=",", fmt='%i')
     
